e231a5c57d072f45dd8062050ffb4073a5c2cfa2
kelvi s. padilla <kurtkelvisk88@gmail.com>
2015-01-12 16:35:39 -0600

    se agrego pager adapter

---
+++ app/src/main/java/com/test/kesmile/materialdesign/adapters/PagerAdapter.java
@@ -0,0 +1,25 @@
+package com.test.kesmile.materialdesign.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by root on 12/01/15.
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
