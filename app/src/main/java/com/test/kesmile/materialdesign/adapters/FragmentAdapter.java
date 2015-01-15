5e482dd53cd4df50e2e0cfaf6b1e845ce9447ed1
kelvi s. padilla <kurtkelvisk88@gmail.com>
2015-01-15 12:22:50 -0600

    Ejemplo de listview con adapter personalizado

--- app/src/main/java/com/test/kesmile/materialdesign/adapters/FragmentAdapter.java
+++ app/src/main/java/com/test/kesmile/materialdesign/adapters/FragmentAdapter.java
@@ -1,9 +1,15 @@
 package com.test.kesmile.materialdesign.adapters;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.test.kesmile.materialdesign.R;
 import com.test.kesmile.materialdesign.beans.BeanFragment;
 
 import java.util.ArrayList;
@@ -13,23 +19,51 @@ import java.util.ArrayList;
  */
 public class FragmentAdapter extends BaseAdapter {
     private ArrayList<BeanFragment> listabeans;
+    private LayoutInflater linflate;
+
+    public FragmentAdapter( Context c,ArrayList<BeanFragment> listabeans) {
+        this.listabeans = listabeans;
+        this.linflate = LayoutInflater.from(c);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return listabeans.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+
+        return listabeans.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        viewholder holder;
+            if(convertView == null){
+                convertView = this.linflate.inflate(R.layout.listview_adapter_fragment_main,parent,false);
+                holder = new viewholder();
+                holder.titulo = (TextView) convertView.findViewById(R.id.txt_Titulo);
+                holder.descripcion = (TextView) convertView.findViewById(R.id.txt_desc);
+                holder.imagen = (ImageView) convertView.findViewById(R.id.imageView);
+            }else{
+                holder = (viewholder) convertView.getTag();
+            }
+                BeanFragment d = listabeans.get(position);
+                holder.titulo.setText(d.getTitulo());
+                holder.descripcion.setText(d.getDescription());
+                holder.imagen.setImageBitmap(d.getImagen());
+            return convertView;
+    }
+    public static class viewholder{
+        TextView titulo;
+        TextView descripcion;
+        ImageView imagen;
     }
 }
--- app/src/main/java/com/test/kesmile/materialdesign/fragments/FragmentMainTest.java
+++ app/src/main/java/com/test/kesmile/materialdesign/fragments/FragmentMainTest.java
@@ -1,5 +1,7 @@
 package com.test.kesmile.materialdesign.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.test.kesmile.materialdesign.R;
+import com.test.kesmile.materialdesign.adapters.FragmentAdapter;
+import com.test.kesmile.materialdesign.beans.BeanFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by root on 8/01/15.
@@ -27,9 +33,22 @@ public class FragmentMainTest extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        String[] data = new String[]{"uno","dos","tres","cuatro"};
+        ArrayList<BeanFragment> beans = new ArrayList<BeanFragment>();
+        BeanFragment obj1 = new BeanFragment();
+        obj1.setTitulo("Hola mundo");
+        obj1.setDescription("Esta es una descripcion general");
+        obj1.setImagen(BitmapFactory.decodeResource(getResources(),R.drawable.img));
+        beans.add(obj1);
+        BeanFragment obj2 = new BeanFragment();
+        obj2.setTitulo("Hola mundo");
+        obj2.setDescription("Esta es una descripcion general");
+        obj2.setImagen(BitmapFactory.decodeResource(getResources(),R.drawable.img));
+        beans.add(obj2);
+        FragmentAdapter adapter = new FragmentAdapter(getActivity().getApplicationContext(),
+                                                      beans);
+        /*String[] data = new String[]{"uno","dos","tres","cuatro"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
-                                                               android.R.layout.simple_list_item_1,data);
+                                                               android.R.layout.simple_list_item_1,data);*/
         lista.setAdapter(adapter);
     }
 }
